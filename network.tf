5a818fd8bef6fd7a4a1259636bdbc8dd8fb9b674
Maruf Salaudeen <murphyola112@gmail.com>
2026-07-05 11:29:29 +0000

    Create public network infrastructure with Terraform

--- network.tf
+++ network.tf
@@ -10,3 +10,50 @@ resource "aws_vpc" "main" {
     ManagedBy   = "Terraform"
   }
 }
+
+resource "aws_subnet" "public" {
+  vpc_id                  = aws_vpc.main.id
+  cidr_block              = "10.0.1.0/24"
+  availability_zone       = "eu-north-1a"
+  map_public_ip_on_launch = true
+
+  tags = {
+    Name        = "public-subnet"
+    Environment = "Production"
+    Project     = "aws-terraform-production-infrastructure"
+    ManagedBy   = "Terraform"
+  }
+}
+
+
+resource "aws_internet_gateway" "igw" {
+  vpc_id = aws_vpc.main.id
+
+  tags = {
+    Name        = "production-igw"
+    Environment = "Production"
+    Project     = "aws-terraform-production-infrastructure"
+    ManagedBy   = "Terraform"
+  }
+}
+
+resource "aws_route_table" "public" {
+  vpc_id = aws_vpc.main.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.igw.id
+  }
+
+  tags = {
+    Name        = "public-route-table"
+    Environment = "Production"
+    Project     = "aws-terraform-production-infrastructure"
+    ManagedBy   = "Terraform"
+  }
+}
+
+resource "aws_route_table_association" "public" {
+  subnet_id      = aws_subnet.public.id
+  route_table_id = aws_route_table.public.id
+}
